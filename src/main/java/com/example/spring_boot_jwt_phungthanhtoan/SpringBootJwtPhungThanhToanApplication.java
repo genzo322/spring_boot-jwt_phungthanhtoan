a1d864fbc718e8398f11dc55df96bbaaf03a1870
Phung Thanh Toan <toanpt322@gmail.com>
2021-09-23 11:02:24 +0700

    spring_boot_jwt_PhungThanhToan

---
+++ src/main/java/com/example/spring_boot_jwt_phungthanhtoan/SpringBootJwtPhungThanhToanApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_phungthanhtoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtPhungThanhToanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtPhungThanhToanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_boot_jwt_phungthanhtoan/SpringBootJwtPhungThanhToanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_phungthanhtoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtPhungThanhToanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
